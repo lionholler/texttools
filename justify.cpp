4f975dead481f97c16ba186e967d18141bcdaefc
Lion Holler <82542096+lionholler@users.noreply.github.com>
2024-01-18 10:22:13 +0100

    compact
    compact

--- justify.cpp
+++ justify.cpp
@@ -10,78 +10,58 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 	const int LIMIT = stoi(argv[1]);
-	const string IN = argv[2];
-
-	vector<string> a;
-	string t;
-	for (int i = 0; i < IN.size(); i++) {
-		if (IN[i] == ' ') {
-			if (t.size() > LIMIT) {
-				cout << "[ERROR] specify a higher limit." << endl;
-				return -1;
-			}
-			if (t != "") {
-				a.push_back(t);
-			}
-			t = "";
-			continue;
-		} else if (IN[i] == '\n' || IN[i] == '\r') {
-			if (t != "") {
-				a.push_back(t);
-				t = "";
-			}
-			a.push_back("\n");
-			continue;
-		}
-		t += IN[i];
-	}
-	a.push_back(t);
+	string IN = argv[2];
 
 	cout << endl;
-	int p = 0;
-	while (p < a.size()) {
-		vector<string> line;
-		int count = 0;
-		int words = 0;
-		int extra_endl = -1;
-		while (count + a[p].size() < LIMIT && p < a.size()) {
-			if (a[p] == "\n") {
-				++p;
-				++extra_endl;
-				break;
-			}
-			line.push_back(a[p]);
-			line.push_back(" ");
-			count += a[p].size() + 1;
-			++p;
-			++words;
-		}
-		count--;
-		words--;
-		if (words > 0) {
-			const int diff = LIMIT - count;
-			const int n = diff / words;
-			const int m = diff % words;
-			int x = 0;
-			for (int i = 0; i < line.size() - 1; i++) {
-				if (line[i] == " ") {
-					for (int j = 0; j < n; j++) {
-						line[i] += ' ';
-					}
-					if (x < m) {
-						line[i] += ' ';
-					}
-					++x;
-				}
-			}
-		}
 
-		for (const auto &i : line) {
-			cout << i;
+	int p = 0;
+	int x = -1;
+	int end;
+	int start = 0;
+	for (int i = 0; i < IN.size(); i++) {
+		if (p == LIMIT || IN[i] == '\n' || i == IN.size()-1) {
+			if (i == IN.size()-1) {
+				end = IN.size();
+				++x;
+			} else if (IN[i] == '\n') {
+				end = i;
+				++x;
+			}
+			if (x > 0) {
+				const int len = end - start;
+				const int miss = LIMIT - len;
+				const int n = miss / x;
+				const int m = miss % x;
+				int y = 0;
+				for (int j = start; j < end; j++) {
+					cout << IN[j];
+					if (IN[j] == ' ') {
+						for (int k = 0; k < n; k++) {
+							cout << ' ';
+						}
+						if (y < m) {
+							cout << ' ';
+						}
+						++y;
+					}
+				}
+				cout << endl;
+			} else {
+				for (int j = start; j < end; j++) {
+					cout << IN[j];
+				}
+				cout << endl;
+			}
+			i = end + 1;
+			start = i;
+			p = 0;
+			x = -1;
 		}
-		cout << endl;
-		for (int i = 0; i < extra_endl; i++) {
-			cout << endl;
+		if (IN[i] == ' ') {
+			end = i;
+			++x;
 		}
+		++p;
 	}
-} 
+
+}
